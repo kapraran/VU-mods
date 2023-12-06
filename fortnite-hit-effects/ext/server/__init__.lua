5540cbb017774755ccf4ea8235eab47c881493d1
FlashHit <56718716+FlashHit@users.noreply.github.com>
2023-12-06 15:00:06 +0100

    add more checks

--- fortnite-hit-effects/ext/server/__init__.lua
+++ fortnite-hit-effects/ext/server/__init__.lua
@@ -1,4 +1,9 @@
 Hooks:Install('Soldier:Damage', 100, function(hookCtx, soldier, info, giverInfo)
+  if info.damage <= 0.0 then return end
+  if not giverInfo.giver then return end
+  if giverInfo.giver.teamId == soldier.teamId then return end
+  if soldier.isManDown then return end
+
   NetEvents:SendToLocal('hit', giverInfo.giver, info.damage, info.boneIndex == 1)
   hookCtx:Pass(soldier, info, giverInfo)
 end)
