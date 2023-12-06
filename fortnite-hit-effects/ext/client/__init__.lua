8b01dffda6befd711b30e9999a7c8c0f932dcab2
FlashHit <56718716+FlashHit@users.noreply.github.com>
2023-12-06 15:01:49 +0100

    add persistent setting to disable/enable the effect

--- fortnite-hit-effects/ext/client/__init__.lua
+++ fortnite-hit-effects/ext/client/__init__.lua
@@ -1,11 +1,11 @@
+local m_Setting = SettingsManager:GetSetting("Enabled") or SettingsManager:DeclareBool("Enabled", true, { displayName = "Enabled", showInUi = true })
+
 Events:Subscribe('Extension:Loaded', function()
   WebUI:Init()
 end)
 
 NetEvents:Subscribe('hit', function(damage, isHeadshot)
-  if damage <= 0 then
-    return
+  if m_Setting.value then
+    WebUI:ExecuteJS(string.format('addHit(%d, %s)', math.floor(damage), tostring(isHeadshot)))
   end
-
-  WebUI:ExecuteJS(string.format('addHit(%d, %s)', math.floor(damage), tostring(isHeadshot)))
 end)
